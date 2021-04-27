bda06d2d0a515b1df820738343f3fd3f89a22307
Charlene Johnson <charcodes24@gmail.com>
2021-04-27 11:35:18 -0700

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const headquarters = 42;
+    if (block >= 42) {
+        return block - headquarters;
+    } else {
+        return headquarters - block;
+    };
+};
+
+function distanceFromHqInFeet(block) {
+    let measure = distanceFromHqInBlocks(block);
+    return measure * 264;
+};
+
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        return (start - end) * 264;
+    } else if (start < end) {
+        return (end - start) * 264;
+    };
+};
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    let price
+    if (distance <= 400) {
+        price = 0;
+        return price;
+    } else if (distance >= 400 && distance <= 2000) {
+        price = (distance - 400) * .02;
+        return price;
+    } else if (distance >= 2000 && distance < 2500) {
+        price = 25;
+        return price;
+    } else if (distance > 2500) {
+        return `cannot travel that far`;
+    }
+};
